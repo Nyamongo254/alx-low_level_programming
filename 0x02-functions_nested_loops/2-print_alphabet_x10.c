687dc8fb3a7cc7067697b8806efd6c74d92ccba8
Nyamongo254 <nestanyamongo46@gmail.com>
2023-06-20 03:21:32 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - Print the  alphabet 10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	char letter;
+	int i = o;
+
+
+	while (i < 10)
+		
+	{ 
+
+	letter = 'a'
+	while (letter <= 'z')
+
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+	i++;
+	}
+}
+
