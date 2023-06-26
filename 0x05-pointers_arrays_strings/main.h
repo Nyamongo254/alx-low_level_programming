52c34cc6c8f5841d114f5015f0f9472eff811d48
Nyamongo254 <nestanyamongo46@gmail.com>
2023-06-26 11:13:07 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int_putchar(char c);
+int _putchar(char c);
 void reset_to_98(int*n);
 void swap_int(int*a, int*b);
 int _strlen(char*s);
-void_puts(char*str);
+void _puts(char*str);
 void print_rev(char*str);
 void rev_string(char*s);
 void puts2(char*str);
