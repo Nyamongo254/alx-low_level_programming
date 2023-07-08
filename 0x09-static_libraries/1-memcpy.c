0d8dcf931d33af2beb3c86d592c76d5159915a1e
Nyamongo254 <nestanyamongo46@gmail.com>
2023-07-08 01:57:41 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
- * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: the desired value
- * @n: number of bytes to be changed
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
  *
- * Return: changed array with new value for n bytes
+ *Return: copied memory with n bytes changed
  */
-char *_memset(char *s, char b, unsigned int n)
+
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-	s[i] = b;
+	dest[i] = src[i];
 	}
-	return (s);
+	return (dest);
 }
